9499279b7ff0ef39b7788acfea03f8450a01161e
batista <batistanostalgia@gmail.com>
2025-10-13 22:01:21 -0300

    initial commit

---
+++ src/main/java/com/backend/musicalta/MusicaltaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.musicalta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicaltaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicaltaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/musicalta/MusicaltaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.musicalta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicaltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
